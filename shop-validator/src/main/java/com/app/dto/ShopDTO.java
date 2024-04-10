2d8f8fc5cfe043dc20d5169aecac9f89fa958814
murilo-scapim <mhscapim@gmail.com>
2024-04-10 17:05:25 -0300

    Add consumer

---
+++ shop-validator/src/main/java/com/app/dto/ShopDTO.java
@@ -0,0 +1,18 @@
+package com.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class ShopDTO {
+
+    private String identifier;
+    private LocalDate dateShop;
+    private String status;
+    private List<ShopItemDTO> items = new ArrayList<>();
+}
---
+++ shop-validator/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create schema if not exists shop;
+
+create table product (
+    id bigserial primary key auto_increment,
+    product_identifier varchar(100) not null,
+    amount int not null
+);
