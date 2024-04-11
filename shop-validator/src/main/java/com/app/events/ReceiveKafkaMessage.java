56b9e0fbdb53f29e71e4f48a9d7a671350436180
murilo-scapim <mhscapim@gmail.com>
2024-04-11 15:28:54 -0300

    Fix shop-validator

--- shop-validator/src/main/java/com/app/events/ReceiveKafkaMessage.java
+++ shop-validator/src/main/java/com/app/events/ReceiveKafkaMessage.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 public class ReceiveKafkaMessage {
 
     private static final String SHOP_TOPIC_NAME = "SHOP_TOPIC";
-
     private static final String SHOP_TOPIC_EVENT_NAME = "SHOP_TOPIC_EVENT";
 
     private final ProductRepository productRepository;
@@ -25,41 +24,34 @@ public class ReceiveKafkaMessage {
 
     @KafkaListener(topics = SHOP_TOPIC_NAME, groupId = "group")
     public void listenShopTopic(ShopDTO shopDTO) {
-        try {
-            log.info("Compra recebida no tópico: {}", shopDTO.getIdentifier());
+        log.info("Compra recebida no tópico: {}", shopDTO.getIdentifier());
 
-            boolean success = true;
-            for (ShopItemDTO item : shopDTO.getItems()) {
-                Product product = productRepository
-                        .findByIdentifier(item.getProductIdentifier());
+        boolean success = true;
+        for (ShopItemDTO item : shopDTO.getItems()) {
+            Product product = productRepository
+                    .findByIdentifier(item.getProductIdentifier());
 
-                if (!isValidShop(item, product)) {
-                    shopError(shopDTO);
-                    success = false;
-                    break;
-                }
+            if (!isValidShop(item, product)) {
+                shopError(shopDTO);
+                success = false;
+                break;
             }
+        }
 
-            if (success) {
-                shopSuccess(shopDTO);
-            }
-        } catch (Exception e) {
-            log.error("Erro no processamento da compra: {}",
-                    shopDTO.getIdentifier());
+        if (success) {
+            shopSuccess(shopDTO);
         }
     }
 
-    private boolean isValidShop(
-            ShopItemDTO item,
-            Product product) {
-        return product != null ||
+    private boolean isValidShop(ShopItemDTO item, Product product) {
+        return product != null &&
                 product.getAmount() >= item.getAmount();
     }
 
     // Envia uma mensagem para o Kafka indicando erro na compra
     private void shopError(ShopDTO shopDTO) {
         log.info("Erro no processamento da compra: {}",
-            shopDTO.getIdentifier());
+                shopDTO.getIdentifier());
         shopDTO.setStatus("ERROR");
         kafkaTemplate.send(SHOP_TOPIC_EVENT_NAME, shopDTO);
     }
--- shop-validator/src/main/resources/schema.sql
+++ shop-validator/src/main/resources/schema.sql
@@ -2,6 +2,6 @@ create schema if not exists shop;
 
 create table product (
     id bigserial primary key auto_increment,
-    product_identifier varchar(100) not null,
+    identifier varchar(100) not null,
     amount int not null
 );
